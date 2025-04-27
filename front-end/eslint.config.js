a79c79153e2017831be9528e62cfcf7471f14946
laurabgauvin <68168499+laurabgauvin@users.noreply.github.com>
2025-04-27 12:33:56 -0600

    Update eslint config

--- front-end/eslint.config.js
+++ front-end/eslint.config.js
@@ -2,6 +2,7 @@ const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
 const prettier = require('eslint-config-prettier');
+const jsdoc = require('eslint-plugin-jsdoc');
 
 module.exports = tseslint.config(
     {
@@ -18,12 +19,84 @@ module.exports = tseslint.config(
             ...tseslint.configs.strictTypeChecked,
             ...tseslint.configs.stylisticTypeChecked,
             ...angular.configs.tsRecommended,
+            jsdoc.configs['flat/recommended-typescript'],
+            jsdoc.configs['flat/logical-typescript'],
             prettier,
         ],
         processor: angular.processInlineTemplates,
         rules: {
-            '@typescript-eslint/no-confusing-void-expression': 'off',
-            '@typescript-eslint/no-extraneous-class': 'off',
+            'no-console': ['error', { allow: ['error'] }],
+            'prefer-template': 'error',
+            'no-else-return': 'error',
+            'no-lonely-if': 'error',
+            eqeqeq: ['error', 'always'],
+            '@typescript-eslint/no-extraneous-class': [
+                'error',
+                {
+                    allowWithDecorator: true,
+                },
+            ],
+            '@typescript-eslint/restrict-template-expressions': [
+                'error',
+                {
+                    allowBoolean: true,
+                    allowNullish: true,
+                    allowNumber: true,
+                },
+            ],
+            '@typescript-eslint/prefer-readonly': 'warn',
+            '@typescript-eslint/no-unused-vars': 'warn',
+            '@typescript-eslint/explicit-function-return-type': 'error',
+            '@typescript-eslint/naming-convention': [
+                'error',
+                {
+                    selector: 'default',
+                    format: ['camelCase'],
+                    leadingUnderscore: 'allow',
+                    trailingUnderscore: 'allow',
+                },
+                {
+                    selector: 'variable',
+                    format: ['camelCase', 'UPPER_CASE'],
+                    leadingUnderscore: 'allow',
+                    trailingUnderscore: 'allow',
+                },
+                {
+                    // private variables and functions must have a leading underscore
+                    selector: 'memberLike',
+                    modifiers: ['private'],
+                    format: ['camelCase'],
+                    leadingUnderscore: 'require',
+                },
+                {
+                    // type and interface names must be pascalcase
+                    selector: 'typeLike',
+                    format: ['PascalCase'],
+                },
+                {
+                    // interface name cannot start with I
+                    selector: 'interface',
+                    format: ['PascalCase'],
+                    custom: {
+                        regex: '^I[A-Z]',
+                        match: false,
+                    },
+                },
+                {
+                    // enum values must be pascalcase
+                    selector: 'enumMember',
+                    format: ['PascalCase'],
+                },
+                {
+                    // global const variables must be in uppercase
+                    selector: 'variable',
+                    modifiers: ['const', 'global'],
+                    format: ['UPPER_CASE'],
+                },
+            ],
+            'jsdoc/require-returns': 'off',
+            'jsdoc/tag-lines': ['warn', 'always', { count: 0, startLines: 1 }],
+            'jsdoc/require-param-description': 'off',
             '@angular-eslint/directive-selector': [
                 'error',
                 {
